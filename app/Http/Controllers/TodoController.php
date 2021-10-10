cdf6723cb8a92b5c5e287349edb03fba301179ab
csetowhid <towhidhasang1@gmail.com>
2021-10-10 15:27:31 -0600

    Shown

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -23,6 +23,7 @@ class TodoController extends Controller
      */
     public function index()
     {
+        /*---------------------------------------------------------
         // $data['list'] = Todo::select(['id','task'])->paginate(2);
         // $data['list'] = Todo::orderBy('id','desc')->paginate(2);
         // $data['list'] = Todo::latest()->paginate(2);
@@ -33,6 +34,8 @@ class TodoController extends Controller
         // $data['list']->dump();
         // $data['list'] = Todo::withTrashed()->paginate(2);
         // $data['list'] = Todo::onlyTrashed()->paginate();
+
+        .................................*/
 
         $data['list'] = Todo::with('Categories')->paginate(5);
         $data['users'] = User::count('id');
